81dbde38cab71850378a63b0d184a80da194087d
hellnomary <maria.porcires@outlook.com>
2023-08-17 14:26:06 +0200

    update

--- zebra-project/src/App.tsx
+++ zebra-project/src/App.tsx
@@ -6,24 +6,35 @@ import { Data } from './interfaces'
 import { StopLocation, Coords } from "./interfaces";
 
 
-
 function App() {
   const [position, setPosition] = useState<Coords | null>(null)
   const [station, setStation] = useState<Data | null>(null)
+  const [departure, setDeparture] = useState('')
 
- let stops: null | StopLocation[] = null;
- if (station) {
-  stops = station.stopLocationOrCoordLocation
- }
+  function handleClick() {
+    
+  }
+
 
   return (
     <div>
 
-    <button onClick={() => getPosition(setPosition)}> See location </button>
-    <p>Your position is {position?.lat} {position?.lon}</p>
-   
-    <button onClick={() =>reverseGeocode(lat, lon, setStation)}>Stations</button>
-    <p>{ station }</p>
+      <button onClick={() => getPosition(setPosition)}> See location </button>
+      <p>Your position is {position?.lat} {position?.lon}</p>
+    
+    {position ? 
+      <button onClick={() =>reverseGeocode(position.lat, position.lon, setStation)}>Stations</button>
+    : <p> hej </p>
+    }
+      {station ?
+      station.stopLocationOrCoordLocation.map(stop => (
+      <ul> 
+          <button onClick={handleClick} key={stop.StopLocation.extId}>{stop.StopLocation.name}{stop.StopLocation.dist}</button>
+      </ul>
+      ))
+    : <p>ingen station</p>
+    }
+
     </div>
   )
 }
